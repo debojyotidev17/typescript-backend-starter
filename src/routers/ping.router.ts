1a73fff35a23e55d026cc807cad44a1f07badd6e
“Debojyot <debojyoti.devx@gmail.com>
2026-08-19 18:13:43 +0530

    add: zod validation

--- src/routers/ping.router.ts
+++ src/routers/ping.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { pingHandler } from "../controllers/ping.controller.js";
+import { pingSchema } from "../validators/ping.validator.js";
+import { validateRequestBody } from "../validators/index.js";
 
 const pingRouter = express.Router();
 
-pingRouter.get('/', pingHandler);
+pingRouter.get("/", validateRequestBody(pingSchema), pingHandler);
 
 export default pingRouter;
-
---
+++ src/validators/index.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+/**
+ * @params schema = zod schema to validate the req body
+ * @returns = middleware function to validate the req body
+ */
+
+export const validateRequestBody = (schema: z.ZodType) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.parseAsync(req.body);
+            console.log("Request body is valid");
+            next();
+        } catch (error) {
+            // if the validation fails
+            return res.json({
+                message: "Invalid request body",
+                success: false,
+                error: error,
+            });
+        }
+    };
+};
